77972f2cc6837ffb3ee46b2b81de8126a8a8ffba
autocommit <autocommit>
2017-08-09 20:44:23 +0000

    auto 0 14 178

--- src/test_copynode.c
+++ src/test_copynode.c
@@ -1,5 +1,6 @@
 #include <libxml/tree.h> // xmlNode*
 #include <stdio.h>
+#include <assert.h>
 
 #define LITLEN(a) (a),sizeof(a)-1
 int main(int argc, char *argv[])
@@ -14,9 +15,10 @@ int main(int argc, char *argv[])
 																 "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">"
 																	 "<html xmlns=\"http://www.w3.org/1999/xhtml\"><head></head></html>"));
 	xmlDocDump(stdout,other);
-	xmlNode* copy = xmlDocCopyNode(doc->children->children, other, 1);
+	assert(doc->children);
+	xmlNode* copy = xmlDocCopyNode(doc->children, other, 1);
 	xmlAddChild(other->children, copy);
-	printf("um %p\n",copy);
+	printf("um %s\n",copy->name);
 	puts("--------");
 	xmlDocDump(stdout,other);
 	return 0;
