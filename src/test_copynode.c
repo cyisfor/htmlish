96cc2b20dc431847b360aa4c687230abb5604bac
autocommit <autocommit>
2017-08-09 20:34:15 +0000

    auto 0 26 462

--- src/test_copynode.c
+++ src/test_copynode.c
@@ -1,5 +1,18 @@
+#include <libxml/tree.h> // xmlNode*
+#include <stdio.h>
+
+
 int main(int argc, char *argv[])
 {
-	
+	xmlDoc* doc = xmlParseMemory(LITLEN(
+																 "<?xml charset=\"utf-8\"?>\n<xml><thing></thing></xml>"));
+	xmlDocDump(stdout,doc);
+	xmlDoc* other = xmlParseMemory(LITLEN(
+																 "<?xml charset=\"utf-8\"?>\n<xml><thing2></thing2></xml>"));
+	xmlDocDump(stdout,other);
+	xmlNode* copy = xmlDocCopyNode(doc->root, other, 1);
+	xmlAddChild(other->root, copy);
+	puts("--------");
+	xmlDocDump(stdout,other);
 	return 0;
 }
