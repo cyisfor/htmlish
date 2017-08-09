e20876599b86d30f3c4e5ec727374b3e83fc14c9
autocommit <autocommit>
2017-08-09 20:41:01 +0000

    auto 0 17 425

--- src/test_copynode.c
+++ src/test_copynode.c
@@ -5,7 +5,9 @@
 int main(int argc, char *argv[])
 {
 	xmlDoc* doc = xmlParseMemory(LITLEN(
-																 "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<html xmlns=\"http://www.w3.org/1999/xhtml\"><body></body></html>"));
+																 "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
+																 "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">"
+																 "<html xmlns=\"http://www.w3.org/1999/xhtml\"><body></body></html>"));
 	xmlDocDump(stdout,doc);
 	xmlDoc* other = xmlParseMemory(LITLEN(
 																 "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<xml xmlns=\"derp\"><thing2></thing2></xml>"));
