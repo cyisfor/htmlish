3d8d0f2e39d2c32660568fc6fd9c7cc1ae9196cb
autocommit <autocommit>
2017-08-10 00:34:43 +0000

    auto 0 8 111

--- src/main.c
+++ src/main.c
@@ -126,7 +126,8 @@ int main(void) {
     LIBXML_TEST_VERSION;
 
 		void on_error(void * userData, xmlErrorPtr error) {
-			fprintf(stderr,"um %s %s\n",error->message,
+			
+			fprintf(stderr,"um %d %s %s\n",error->code, error->message,
 							error->level == XML_ERR_FATAL ? "fatal..." : "ok");
 			return;
 		}
