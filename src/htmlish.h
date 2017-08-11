6f750d10758ce3794d05e1be8de1ec2bc5f54fcc
autocommit <autocommit>
2017-08-11 04:51:08 +0000

    auto 0 10 78

--- src/htmlish.h
+++ src/htmlish.h
@@ -1,5 +1,6 @@
 #include <libxml/tree.h>
 #include <stdbool.h>
 
+void htmlish_str(xmlNode* content, const char* s, size_t l, bool as_children);
 void htmlish(xmlNode* content, int fd, bool as_children);
 xmlNode* getContent(xmlNode* oroot, bool createBody, bool* as_child);
