ca7e463fa0b1de9f4513894cabf27c1a1a09c219
user <nobody>
2017-08-14 03:31:33 +0000

    derp

--- source/libxmlfixes.h
+++
@@ -1,7 +0,0 @@
-#include <libxml/tree.h> // xmlNode
-
-xmlNode* fuckXPath(xmlNode* parent, const char* name);
-xmlNode* fuckXPathDivId(xmlNode* parent, const char* id);
-xmlNode* findOrCreate(xmlNode* parent, const char* path);
-void foreachNode(xmlNode* parent, const char* name, void (*handle)(xmlNode*,void*), void* ctx);
-xmlDoc* readFunky(int fd, const char* content);
