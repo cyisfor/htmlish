6b23f39ea30e4b06c71e3f5b6354bd630f23f355
autocommit <autocommit>
2017-10-05 19:51:41 +0000

    auto 0 25 287

--- src/error.c
+++ src/error.c
@@ -1,5 +1,7 @@
 #include "htmlish.h"
 #include "html_when.h"
+#define LITLEN(a) a,(sizeof(a)-1)
+
 #include "libxmlfixes/wanted_tags.h"
 #include <libxml/xmlerror.h>
 #include <string.h>
@@ -9,7 +11,15 @@
 
 bool htmlish_handled_error(xmlErrorPtr error) {
 	if(html_when_handled_error(error)) return true;
-	if(error->code == XML_HTML_UNKNOWN_TAG) {
+	switch(error->code) {
+	case XML_ERR_ERROR:
+		if(0==strncmp(error->message,LITLEN("htmlParseEntityRef: expecting ';'"))) {
+			// they want us to replace every & with &AMP; in attributes that are
+			// ALREADY DELINEATED BY QUOTATION MARKS
+			return true;
+		}
+		break;
+	case XML_HTML_UNKNOWN_TAG: {
 		const char* name = error->str1;
 		if(lookup_wanted(name) != UNKNOWN_TAG) return true;
 		size_t nlen = strlen(name);
@@ -18,7 +28,9 @@ bool htmlish_handled_error(xmlErrorPtr error) {
 			 || IS("chat"))
 			// not errors, these get removed by template stuffs
 			return true;
+		break;
 	}
+	};
 	fprintf(stderr,"um %d %s %s\n",error->code, error->message,
 					error->level == XML_ERR_FATAL ? "fatal..." : "ok");
 	return false;
